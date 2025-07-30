55d247c84fc11afe3a565da0a23ab40eae1696b9
vamsi mypala <vamsimypala@gmail.com>
2025-07-30 22:53:28 +0530

    mereged

--- task.java
+++ task.java
@@ -1,5 +1,13 @@
 public class task {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Welcome to the Java programming world!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("Let's learn and grow together!");
+        System.out.println("Happy coding!");
+        System.out.println("End of the program.");
+        System.out.println("Goodbye!");
+        System.out.println("Thank you for running this program!");
+        System.out.println("Remember to practice regularly.");
     }
 }
