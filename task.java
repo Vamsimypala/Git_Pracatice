7a16c4f15934cb588b74fbe587aa734d727174df
Vamsimypala <104899504+Vamsimypala@users.noreply.github.com>
2025-07-30 23:09:00 +0530

    in branch change

--- task.java
+++ task.java
@@ -3,11 +3,6 @@ public class task {
         System.out.println("Hello, World!");
         System.out.println("Welcome to the Java programming world!");
         System.out.println("This is a simple Java program.");
-        System.out.println("Let's learn and grow together!");
-        System.out.println("Happy coding!");
-        System.out.println("End of the program.");
-        System.out.println("Goodbye!");
-        System.out.println("Thank you for running this program!");
-        System.out.println("Remember to practice regularly.");
+       
     }
 }
